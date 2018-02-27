e00970f0e2e65566b6ff572197007a98ac9e035d
jurabek <jurabek.azizkhujaev@gmail.com>
2018-02-28 00:07:28 +0200

    identity changes

---
+++ src/Services/Autostop.Services.Rides/Controllers/RideHistoryController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Autostop.Services.Rides.Controllers
+{
+    [Route("api/v1/rides/history")]
+    public class RideHistoryController : ControllerBase
+	{
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ src/Services/Autostop.Services.Rides/Models/UserRide.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Autostop.Services.Rides.Models
+{
+    public class UserRide
+    {
+    }
+}
